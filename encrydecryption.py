b6615698a6415804ceb45fc693943084c1e28364
Puspa Regmi <103326765+Puspa222@users.noreply.github.com>
2024-04-17 13:35:08 +0545

    Add files via upload

--- encrydecryption.py
+++ encrydecryption.py
@@ -1,31 +1,54 @@
 #Encryption and decryption Of msg
 import random
 import string
-print("\n\n\n")
-msg=input("Enter Your Msg to Encrypt:")
-res1 = ''.join(random.choices(string.ascii_lowercase +
-                             string.digits, k=2))
-res2= ''.join(random.choices(string.ascii_lowercase +
-                             string.digits, k=3))
-res3 = ''.join(random.choices(string.ascii_lowercase +
-                             string.digits, k=4))
-if len(msg)==1:
-    emsg=res1 +msg+res2
-if len(msg)==2:
-    emsg=res1+''.join(reversed(msg))+res3
-if len(msg)>2:
-    emsg=res3+''.join(reversed(msg[1:]))+res2+msg[0:1]+res1
-print(emsg)
 
-dmsg=input("Enter msg to Decrpty:")
-ddmsg=dmsg
-if len(dmsg)==6:
-    ddmsg=dmsg[2]
-if len(dmsg)==8:
-    ddmsg=''.join(reversed(dmsg[2:4]))
-if len(dmsg)>8:
-    dmsg=dmsg[4:len(dmsg)-2]
-    ddmsg=dmsg[len(dmsg)-1]+''.join(reversed(dmsg[:len(dmsg)-4]))
-else:
-    ddmsg=''.join(random.choices(string.digits,k=2))
-print(ddmsg)
+
+print("\n\n\n")
+str=input("Enter Your Msg to Encrypt:")
+
+
+#Encryption Process
+
+mmsg=str.split()
+res1=''.join(random.choices(string.ascii_lowercase +string.ascii_uppercase+string.digits, k=13 ))
+eemsg=""
+for msg in mmsg:
+   if len(msg)==1:
+       emsg=''.join(random.choices(string.ascii_lowercase +
+                             string.digits, k=2)) +msg+''.join(random.choices(string.ascii_lowercase +
+                             string.digits, k=3))
+   if len(msg)==2:
+       emsg=''.join(random.choices(string.ascii_lowercase +
+                             string.digits, k=2))+''.join(reversed(msg))+ ''.join(random.choices(string.ascii_lowercase +
+                             string.digits, k=4))
+   if len(msg)>2:
+       emsg= ''.join(random.choices(string.ascii_lowercase +
+                             string.digits, k=4))+''.join(reversed(msg[1:]))+''.join(random.choices(string.ascii_lowercase +
+                             string.digits, k=3))+msg[0:1]+''.join(random.choices(string.ascii_lowercase +
+                             string.digits, k=2))
+   eemsg=eemsg+ res1 +emsg
+
+
+    
+print("\n\n\n Encrypted Msg:\t",eemsg)
+ddmsg=input("\n\nEnter msg to Decrpty:")
+
+#Decryption Process
+
+ddmsg=ddmsg[13:]
+ddmmsg=ddmsg.split(res1)
+dstr=""
+for dmsg in ddmmsg:
+   if len(dmsg)==6:
+       ddmmsg=dmsg[2]
+   elif len(dmsg)==8:
+       ddmmsg=''.join(reversed(dmsg[2:4]))
+   elif len(dmsg)>8:
+       dmsg=dmsg[4:len(dmsg)-2]
+       ddmmsg=dmsg[len(dmsg)-1]+''.join(reversed(dmsg[:len(dmsg)-4]))
+   else:
+       ddmmsg=''.join(random.choices(string.digits,k=2))
+   dstr=dstr+" "+ddmmsg
+
+
+print("\n\n\n Decrypted Msg:\t",dstr)
